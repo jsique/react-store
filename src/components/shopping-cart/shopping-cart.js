bbba6eb8154f439251d6278cc13cdd3356bbe020
jsique <sique.casasola@gmail.com>
2023-03-01 22:44:44 -0600

    shopping car
    template is added to see list of shopping cart proudcts

--- src/components/shopping-cart/shopping-cart.js
+++ src/components/shopping-cart/shopping-cart.js
@@ -2,14 +2,23 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../navBar';
 import { authContext } from "../../services/authContext";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext,useState } from "react";
+import "../../assets/css/shopping_car.css";
 function ShoppingCart(){
 
-    
+    const [totalAmount, settotalAmount] = useState(0);
 	//const { gListShop } = useAuth();
     const { gListShop } = useContext(authContext);
     useEffect(()=>{
-        console.log(gListShop);
+        //console.log(gListShop);
+        let tmpAmount = 0;
+        if(gListShop.length > 0){
+            gListShop.map((p)=>(
+                    tmpAmount += (p.priceProduct*p.quantity)
+                )
+            )
+            settotalAmount(tmpAmount);
+        }
     },[gListShop]);
 
     return(
@@ -24,26 +33,38 @@ function ShoppingCart(){
                 <hr/><br/>
                 <div className="row second_content">
                     <div className="col-6">
-                        <table className="tbl_content" border="1">
-                            <tbody>
-                                <tr className="tbl_content" >
-                                    <td > 
-                                        <img src="" alt="" width="100" height="100" />
-                                    </td>        
-                                </tr>
-                            </tbody> 
-                        </table>    
+                            {
+                                gListShop && 
+                                    gListShop.map((p, index) => (
+                                        <div key={index} className="row border_div">
+                                            <div className="col-sm-3">
+                                                <img src={"/img/" + p.nameImage} alt="" width="100" height="100" />
+                                            </div>
+                                            <div className="col-sm-9">
+                                                <h4>{p.nameProduct}</h4>
+                                                <strong>Unidades: </strong> <label >{p.quantity}</label><br/>
+                                                <strong>Sub total: </strong> <label >${p.priceProduct*p.quantity}</label>   
+                                            </div>
+                                        </div>
+                                        // <tr key={p.idProduct} className="tbl_content" >
+                                        //     <td key={p.nameProduct}> 
+                                        //         <img src={"/img/" + p.nameImage} alt="" width="100" height="100" />
+                                        //     </td>     
+                                        //     <h4 key={p.nameImage}>{p.nameProduclt}</h4>
+                                        //     <strong>Unidades: </strong> <label for="">{p.quantity}</label><br/>
+                                        //     <strong>Sub total: </strong> <label for="">${p.priceProduct*p.quantity}</label>   
+                                        // </tr>
+                                        // <p key={p.idProduct}>{p.nameProduct}</p>
+                                    )
+                                )
+                            }
                     </div>
-
                     <div className="col-6">
-                        <h3>Total: $ </h3>
+                        <h3>Total: ${totalAmount} </h3>
                         <button type="button" className="btn btn-light btn_cart"  >Cancelar</button>
                         <button type="button" className="btn btn-light btn_cart" >Pagar</button>
                     </div>
                 </div>
-                {gListShop && gListShop.map((p) => (
-                        <p key={p.idProduct}>{p.nameProduct}</p>
-                    ))}
             </div>
         </div>
     )
